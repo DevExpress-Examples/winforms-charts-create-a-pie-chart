96f9caef8a783673c3c321d9d0d4b3bb4a847109
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Series_PieChart/Form1.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Windows.Forms;
+using DevExpress.XtraCharts;
+// ...
+
+namespace Series_PieChart {
+    public partial class Form1 : Form {
+        public Form1 () {
+            InitializeComponent();
+        }
+
+        private void Form1_Load (object sender, EventArgs e) {
+            // Create an empty chart.
+            ChartControl pieChart = new ChartControl();
+
+            // Create a pie series.
+            Series series1 = new Series("A Pie Series", ViewType.Pie);
+
+            // Populate the series with points.
+            series1.Points.Add(new SeriesPoint("Russia", 17.0752));
+            series1.Points.Add(new SeriesPoint("Canada", 9.98467));
+            series1.Points.Add(new SeriesPoint("USA", 9.63142));
+            series1.Points.Add(new SeriesPoint("China", 9.59696));
+            series1.Points.Add(new SeriesPoint("Brazil", 8.511965));
+            series1.Points.Add(new SeriesPoint("Australia", 7.68685));
+            series1.Points.Add(new SeriesPoint("India", 3.28759));
+            series1.Points.Add(new SeriesPoint("Others", 81.2));
+
+            // Add the series to the chart.
+            pieChart.Series.Add(series1);
+
+            // Adjust the point options of the series.
+            series1.Label.PointOptions.PointView = PointView.ArgumentAndValues;
+            series1.Label.PointOptions.ValueNumericOptions.Format = NumericFormat.Percent;
+            series1.Label.PointOptions.ValueNumericOptions.Precision = 0;
+
+            // Detect overlapping of series labels.
+            ((PieSeriesLabel)series1.Label).ResolveOverlappingMode = ResolveOverlappingMode.Default;
+
+            // Adjust the position of series labels. 
+            ((PieSeriesLabel)series1.Label).Position = PieSeriesLabelPosition.TwoColumns;
+
+            // Access the view-type-specific options of the series.
+            PieSeriesView myView = (PieSeriesView)series1.View;
+
+            // Show a title for the series.
+            myView.Titles.Add(new SeriesTitle());
+            myView.Titles[0].Text = series1.Name;
+
+            // Specify a data filter to explode points.
+            myView.ExplodedPointsFilters.Add(new SeriesPointFilter(SeriesPointKey.Value_1,
+                DataFilterCondition.GreaterThanOrEqual, 9));
+            myView.ExplodedPointsFilters.Add(new SeriesPointFilter(SeriesPointKey.Argument,
+                DataFilterCondition.NotEqual, "Others"));
+            myView.ExplodeMode = PieExplodeMode.UseFilters;
+            myView.ExplodedDistancePercentage = 30;
+            myView.RuntimeExploding = true;
+            myView.HeightToWidthRatio = 0.75;
+
+            // Hide the legend (if necessary).
+            pieChart.Legend.Visible = false;
+
+            // Add the chart to the form.
+            pieChart.Dock = DockStyle.Fill;
+            this.Controls.Add(pieChart);
+        }
+    }
+}

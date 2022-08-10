d0b16c33b3981ed051bf82b9599e774a276e4f5c
DevExpressExampleBot <fakeEmail>
2022-08-10 10:09:35 +0400

    Source auto update [skip ci]

--- VB/Series_PieChart/Form1.vb
+++ VB/Series_PieChart/Form1.vb
@@ -1,23 +1,22 @@
-﻿Imports System
+Imports System
 Imports System.Windows.Forms
 Imports DevExpress.XtraCharts
-' ...
 
+' ...
 Namespace Series_PieChart
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
 
         Public Sub New()
             InitializeComponent()
         End Sub
 
-        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
             ' Create an empty chart.
-            Dim pieChart As New ChartControl()
-
+            Dim pieChart As ChartControl = New ChartControl()
             ' Create a pie series.
-            Dim series1 As New Series("A Pie Series", ViewType.Pie)
-
+            Dim series1 As Series = New Series("A Pie Series", ViewType.Pie)
             ' Populate the series with points.
             series1.Points.Add(New SeriesPoint("Russia", 17.0752))
             series1.Points.Add(New SeriesPoint("Canada", 9.98467))
@@ -27,26 +26,19 @@ Namespace Series_PieChart
             series1.Points.Add(New SeriesPoint("Australia", 7.68685))
             series1.Points.Add(New SeriesPoint("India", 3.28759))
             series1.Points.Add(New SeriesPoint("Others", 81.2))
-
             ' Add the series to the chart.
             pieChart.Series.Add(series1)
-
             ' Format the the series labels.
             series1.Label.TextPattern = "{A}: {VP:p0}"
-
             ' Adjust the position of series labels. 
             CType(series1.Label, PieSeriesLabel).Position = PieSeriesLabelPosition.TwoColumns
-
             ' Detect overlapping of series labels.
             CType(series1.Label, PieSeriesLabel).ResolveOverlappingMode = ResolveOverlappingMode.Default
-
             ' Access the view-type-specific options of the series.
             Dim myView As PieSeriesView = CType(series1.View, PieSeriesView)
-
             ' Show a title for the series.
             myView.Titles.Add(New SeriesTitle())
             myView.Titles(0).Text = series1.Name
-
             ' Specify a data filter to explode points.
             myView.ExplodedPointsFilters.Add(New SeriesPointFilter(SeriesPointKey.Value_1, DataFilterCondition.GreaterThanOrEqual, 9))
             myView.ExplodedPointsFilters.Add(New SeriesPointFilter(SeriesPointKey.Argument, DataFilterCondition.NotEqual, "Others"))
@@ -54,10 +46,8 @@ Namespace Series_PieChart
             myView.ExplodedDistancePercentage = 30
             myView.RuntimeExploding = True
             myView.HeightToWidthRatio = 0.75
-
             ' Hide the legend (if necessary).
             pieChart.Legend.Visibility = DevExpress.Utils.DefaultBoolean.False
-
             ' Add the chart to the form.
             pieChart.Dock = DockStyle.Fill
             Me.Controls.Add(pieChart)
